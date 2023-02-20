929456f17bb4c6ebb26a67b727778f730a368f6b
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-02-20 11:14:42 +0530

    20-02-2023

---
+++ swap_nibble.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class swap_nibble {
+ 
+     
+        static int swapNibbles(int x)
+        {
+            return ((x & 0x0F) << 4 | (x & 0xF0) >> 4);
+        }
+         
+        // Driver code
+        public static void main(String arg[])
+        {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter a number two swap nibbles!");
+            int x = sc.nextInt();
+
+            System.out.print(swapNibbles(x));
+        }
+        
+}
+
