d2a506076e82b508052143a000bd74627541f359
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-02-27 18:10:12 +0530

    27-02-2023

---
+++ BinaryNumberMultiplications.java
@@ -0,0 +1,12 @@
+public class BinaryNumberMultiplications {
+
+        public static void main(String[] args) {
+           int binary1 = 7;
+           int binary2 = 9;
+           int a,b;
+           int multiply = binary1 * binary2;
+
+           System.out.println("Multiplication of " + binary1 + " and " + binary2 + " is: " + multiply);
+        }
+     }
+
