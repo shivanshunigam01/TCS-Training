570bc6c9fdb3e79d4165d5087264ce9ffa53f408
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-03-06 03:16:06 +0530

    06-03-2023

---
+++ x.java
@@ -0,0 +1,14 @@
+class x{
+    void draw(){
+        System.out.println("drawing...");}
+    }
+        class Rectangle extends x{
+            void draw() {
+                System.out.println("drawingrectangle...");} 
+            }    class Circle extends x{
+                void draw(){
+                    System.out.println("drawingcircle...");}
+                
+                }
+                class Triangle extends x{void draw()
+                    {System.out.println("drawingtriangle...");}}
