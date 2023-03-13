37937c202d771b51043247c3659ccd7542201ebb
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-03-14 02:56:15 +0530

    14-03-2023

---
+++ java.java
@@ -0,0 +1,10 @@
+public class java {
+    // public class Solution{
+        public static void main(String[] args){
+            byte x = 127;
+            x++;
+            x++;
+            System.out.print(x);
+        }
+ }
+
