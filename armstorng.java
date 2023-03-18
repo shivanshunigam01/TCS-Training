6d3ca233e3790aecce1e8a16c720155f54e28f0d
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-03-18 14:25:09 +0530

    18-03-2023

--- armstorng.java
+++ armstorng.java
@@ -1,41 +1,41 @@
-import java.util.Scanner;
+        import java.util.Scanner;
 
-public class armstorng {
-   public static void main(String[] args) {
-      Scanner input = new Scanner(System.in);
-      System.out.print("Enter the 1st range: ");
-      int lowerLimit = input.nextInt();
-      System.out.print("Enter the 2nd range: ");
-      int upperLimit = input.nextInt();
-      input.close();
+        public class armstorng {
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter the 1st range: ");
+            int lowerLimit = input.nextInt();
+            System.out.print("Enter the 2nd range: ");
+            int upperLimit = input.nextInt();
+            input.close();
 
-      for (int i = lowerLimit; i <= upperLimit; i++) {
-         if (isArmstrong(i)) {
-            System.out.println(" The armstrong numbers are: "+i);
-          
-         }
-      }
-   }
+            for (int i = lowerLimit; i <= upperLimit; i++) {
+                if (isArmstrong(i)) {
+                    System.out.println(" The armstrong numbers are: "+i);
+                
+                }
+            }
+        }
 
-   public static boolean isArmstrong(int n) {
-      int originalNumber, remainder, result = 0;
-      originalNumber = n;
+        public static boolean isArmstrong(int n) {
+            int originalNumber, remainder, result = 0;
+            originalNumber = n;
 
-      while (originalNumber != 0) {
-         remainder = originalNumber % 10;
-         result += Math.pow(remainder, countDigits(n));
-         originalNumber /= 10;
-      }
+            while (originalNumber != 0) {
+                remainder = originalNumber % 10;
+                result += Math.pow(remainder, countDigits(n));
+                originalNumber /= 10;
+            }
 
-      return result == n;
-   }
+            return result == n;
+        }
 
-   public static int countDigits(int n) {
-      int count = 0;
-      while (n != 0) {
-         count++;
-         n /= 10;
-      }
-      return count;
-   }
-}
+        public static int countDigits(int n) {
+            int count = 0;
+            while (n != 0) {
+                count++;
+                n /= 10;
+            }
+            return count;
+        }
+        }
